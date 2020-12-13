39c63ddfd0252e32397f295cbd40491b4bc4d812
alexandra-oras-selab <larisa.oras@mta.ro>
2020-12-13 23:31:11 +0200

    Actualizare-clasa-Crawler
    Issue: #34
    
    Signed-off-by: alexandra-oras-selab <larisa.oras@mta.ro>

--- src/Crawler.java
+++ src/Crawler.java
@@ -6,22 +6,19 @@ public class Crawler {
 
         CrawlerConfig config;
 
-        if(args[0].equals("crawl")){
-            //crawler crawl config.conf;
-            config = new CrawlerConfig(args[1]);
+       if(args[0].equals("crawl")){
+        
+//            config = new CrawlerConfig(args[1]);
+//
+//            CrawlURL firstCrawl = new CrawlURL(args[2], "sites");
+//            firstCrawl.startCrawl();
 
-            CrawlURL firstCrawl = new CrawlURL(args[2], "sites");
-            firstCrawl.startCrawl();
-
-        } else if(args[0].equals("list")){
-            //crawler list png
-            //parcurgere fisiere din sitemap
-            //si afisare doar acele fisiere cu tipul specificat(ex: png)
+            var crawler = new CrawlerInDepth(3,"https://www.mta.ro");
+            crawler.startCrawl();
 
         } else if(args[0].equals("search")){
-            //crawler search document
-            //parcurgere fisiere din sitemap
-            //si afisare doar acele fisiere cu numele specificat(ex: search)
+           Cautare cautare=new Cautare();
+            cautare.cauta("precum");
 
         } else if(args[0].equals("filter")){
             CrawlerFilter crawlerFilter = new CrawlerFilter(args[1],"sites/"+args[2]);
